dd1cc1e148aeab345e788661b10a039abf001417
jacky6632003 <jacky6632003@gmail.com>
2020-03-01 17:32:51 +0800

    加入專案檔案。

---
+++ SampleWithUnity/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Web.Mvc;
+using AutoMapper;
+using SampleWithUnity.Service;
+
+namespace SampleWithUnity.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly IMapper _mapper;
+
+        public HomeController(
+            IProductService productService,
+            IMapper mapper)
+        {
+            this._productService = productService;
+            this._mapper = mapper;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var wording = string.Join(",", this._productService.GetAll());
+
+            ViewBag.Message = $"Your application description page. : {wording}";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
